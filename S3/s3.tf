67d975b53434a2e42a5dc2cc05245efb370d9ded
Yura <shupik3@gmail.com>
2024-02-18 16:03:31 +0200

    S3

---
+++ S3/s3.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region = "eu-central-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my_bucket"
+  tags = {
+    Name = "My Terraform Bucket"
+  }
+}
+
+resource "aws_s3_bucket_object" "example_object_folder" {
+  bucket = aws_s3_bucket.my_bucket.id
+  key = "myfolder-test/"
+  content = ""
+  tags = {
+    Name = "My Folder"
+  }
+}
+
+resource "aws_s3_bucket_object" "example_object_image" {
+  bucket = aws_s3_bucket.my_bucket.id
+  key    = "myfolder-test/devops.jpg"
+  source = "https://github.com/Cyber1993/TERRAFORM_JENKINS/blob/main/install.sh"
+  tags = {
+    Name = "devops.jpg Image"
+  }
+}
