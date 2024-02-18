e8a4fab5d4b506f64d9b27da38dd04c913f4261f
Yura <shupik3@gmail.com>
2024-02-18 09:37:32 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,1 @@
+touch /home/ubuntu/1.txt
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "aws_access_key" {
+  type = string
+  default = "AKIA3JWEQ5JLUJF2CCFO"
+}
+variable "aws_secret_key" {
+  type = string
+  default = "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"
+}
