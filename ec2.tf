cf8ae2f3e257b69ee4c9602fc3cdf2804993c491
Yura <shupik3@gmail.com>
2024-02-18 09:35:43 +0200

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region = "eu-north-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+resource "aws_instance" "nmd221" {
+  availability_zone = "eu-north-1a"
+  ami = "ami-0014ce3e52359afbd"
+  instance_type = "t3.micro"
+  key_name = "221"
+  vpc_security_group_ids = [aws_security_group.default.id]
+  ebs_block_device {
+    device_name = "/dev/sda1"
+    volume_size = 15
+    volume_type = "standard"
+  }
+  user_data = file("install.sh")
+  tags = {
+    Name = "EC2-instance"
+  }
+}
+
+resource "aws_security_group" "default" {
+  ingress{
+    description = "Allow SSH"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress{
+    description = "Allow HTTP"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress{
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
